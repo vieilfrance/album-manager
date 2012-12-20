6be0e8d034b5bed18833887b39023b019cfc7674
David Beck <dbec.pro@gmail.com>
2012-12-20 23:42:58 +0100

    0000015: Easyupload essaye de s'occuper des repertoires pour créer des photos

--- admin/easyupload.php
+++ admin/easyupload.php
@@ -116,11 +116,12 @@ if (is_dir($repertoire."/easyupload")) // Test s'il s'agit d'un repertoire
 	$dir = opendir($repertoire."/easyupload"); //ouvre le repertoire courant d?gn?ar la variable
 	$compteur=-1;
 	while(false!==($file = readdir($dir)))
-		{ //on lit tout et on recupere tout les fichiers dans $file
-
-		if(!in_array($file, array('.','..','Thumbs.db','thumbs.db'))) //on enleve le parent et le courant '. et ..'
+		{ //on lit tout et on recupere tous les fichiers dans $file 
+		if (is_dir($repertoire."/easyupload/".$file) == false) // on filtre les repertoire - hack : il faut mettre le full path sinon le test ne fonctionne pas
+		{
+		echo "==>".$file;
+		if(!in_array($file, array('.','..','Thumbs.db','thumbs.db')) ) //on enleve le parent et le courant '. et ..'
 			{ 
-			$compteur=$compteur+1;
 			$page = $file; 
 			$page = explode('.', $page); //sort l'extension du fichier
 			$nb = count($page);
@@ -143,41 +144,44 @@ if (is_dir($repertoire."/easyupload")) // Test s'il s'agit d'un repertoire
 				$ext_fichier = '';
 				}
 				
-			if($ext_fichier != 'php' and $ext_fichier != 'html' and $ext_fichier != 'db') 
+			if($ext_fichier != 'php' and $ext_fichier != 'html' and $ext_fichier != 'db' ) 
 				{ //utile pour exclure certains types de fichiers a ne pas lister
 				// TODO : plutot que d'exclure une liste qui risque de s'etendre, il faudrait mieux inclure la liste des format voulu
+				$compteur=$compteur+1;
 				array_push($fichier, $file);
-				}
 
-		/* test - intégration de la date de prise de vue de la photo */
 
-			$img=$repertoire."/easyupload/".$file;
-			if($exif = exif_read_data($img)) // Si le fichier $img contient des infos Exif
-				{
-				$nom[$compteur]=$fichier[$compteur];
-				$date[$compteur]=$exif['DateTimeOriginal'];
+			/* test - intégration de la date de prise de vue de la photo */
 
-				$rep_temp=explode(":",$date[$compteur]);
-				$nom['album'][$compteur]=$rep_temp[0];
-				$nom['gallerie'][$compteur]=$rep_temp[1];
-				$galleries[$compteur]=$rep_temp[0];  // on stocke les albums dans un tableau
-				if (count($directories[$rep_temp[0]])>0)
+				$img=$repertoire."/easyupload/".$file;
+				if($exif = exif_read_data($img)) // Si le fichier $img contient des infos Exif
 					{
-					if (!in_array($rep_temp[1],$directories[$rep_temp[0]]))
+					$nom[$compteur]=$fichier[$compteur];
+					$date[$compteur]=$exif['DateTimeOriginal'];
+
+					$rep_temp=explode(":",$date[$compteur]);
+					$nom['album'][$compteur]=$rep_temp[0];
+					$nom['gallerie'][$compteur]=$rep_temp[1];
+					$galleries[$compteur]=$rep_temp[0];  // on stocke les albums dans un tableau
+					if (count($directories[$rep_temp[0]])>0)
+						{
+						if (!in_array($rep_temp[1],$directories[$rep_temp[0]]))
+							$directories[$rep_temp[0]][]=$rep_temp[1];
+						}
+					else
+						{
 						$directories[$rep_temp[0]][]=$rep_temp[1];
+						}
+						//$directories[$rep_temp[0]][$compteur]=$rep_temp[1]; // et les galleries dans un autre tableau à double entrée
+					//$directories['gallerie'][$compteur]=$rep_temp[1];
 					}
-				else
-					{
-					$directories[$rep_temp[0]][]=$rep_temp[1];
-					}
-					//$directories[$rep_temp[0]][$compteur]=$rep_temp[1]; // et les galleries dans un autre tableau à double entrée
-				//$directories['gallerie'][$compteur]=$rep_temp[1];
+					else
+						{
+						// TODO : que faire si une photo n'a pas d'info exif pour pouvoir le classer ?
+						}
 				}
-				else
-					{
-					// TODO : que faire si une photo n'a pas d'info exif pour pouvoir le classer ?
-					}
 			}
+		}
 		}
 	}
 
