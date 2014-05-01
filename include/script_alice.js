5f40429c8acc008f430dce8af4d450815e598fbc
David Beck <dbec.pro@gmail.com>
2014-05-01 22:25:35 +0200

    Mise à jour globale

--- include/script_alice.js
+++ include/script_alice.js
@@ -1,3 +1,4 @@
+/*
 jQuery(document).ready(function () {
 	SV.simpleviewer.load('sv-container', '100%', '95%', '804F4A', true);
 });
@@ -10,3 +11,4 @@ params.allowfullscreen = true;
 params.allowscriptaccess = "always";
 params.bgcolor = "804F4A";
 swfobject.embedSWF("simpleviewer.swf", "flashContent", "100%", "95%", "9.0.124", true, flashvars, params);
+*/
